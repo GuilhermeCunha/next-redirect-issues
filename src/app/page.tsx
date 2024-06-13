4d8d5c6efea246d773e187b02d93b502804a5620
Guilherme Cunha <guiscunha@gmail.com>
2024-06-13 08:20:42 -0300

    feat: add link to all redirect ways

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,15 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Link href="/page-2">Go to page 2</Link>
+      <Link href="/redirect/using-response">
+        Test redirecting using NextResponse.redirect(req.url)
+      </Link>
+      <Link href="/redirect/using-redirect">
+        Test redirecting using {"redirect('/')"}
+      </Link>
+      <Link href="/redirect/using-middleware">
+        Test redirecting using NextResponse.redirect(req.url) on the middleware
+      </Link>
     </main>
   );
 }
